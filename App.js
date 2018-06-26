3bca2d47180edebb0606d4b6d9894c1ad9774894
JulienM <julienm@marmelab.com>
2018-06-26 12:10:00 +0200

    Creating basic structure of application

--- App.js
+++ App.js
@@ -1,23 +1,13 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { createStackNavigator } from 'react-navigation';
 
-export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
-    );
-  }
-}
+import HomeScreen from './routes/HomeScreen';
+import GameScreen from './routes/GameScreen';
+import GameListScreen from './routes/GameListScreen';
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+const App = createStackNavigator({
+    Home: { screen: HomeScreen },
+    Game: { screen: GameScreen },
+    GameList: { screen: GameListScreen },
 });
+
+export default App;
